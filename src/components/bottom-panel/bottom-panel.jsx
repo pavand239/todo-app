73d98ca6638cec02620c2b4bd612cc0d8b76eb83
Pavlov Andrey <pavand239@gmail.com>
2020-01-31 18:59:09 +0300

    Add handler fot item status bottoms and bottom panel

--- src/components/bottom-panel/bottom-panel.jsx
+++ src/components/bottom-panel/bottom-panel.jsx
@@ -17,22 +17,22 @@ export default class BottomPanel extends React.Component {
         let {inputValue} = this.state;
         return (
             <div className='d-flex bottom-panel'>
-            <input 
-                className='bottom-panel-input form-control' 
-                type='text' onChange={this.onInputChange} 
-                value={inputValue} 
-                placeholder='Add new item...' />
-            <button 
-                className="btn btn-primary"
-                onClick={()=>{
-                    this.props.handlerOnAddItem(inputValue);
-                    this.setState({
-                        inputValue:''
-                    })
-                }}
-            >
-                Add
-            </button>
+                <input 
+                    className='bottom-panel-input form-control' 
+                    type='text' onChange={this.onInputChange} 
+                    value={inputValue} 
+                    placeholder='Add new item...' />
+                <button 
+                    className="btn btn-primary"
+                    onClick={()=>{
+                        this.props.handlerOnAddItem(inputValue);
+                        this.setState({
+                            inputValue:''
+                        })
+                    }}
+                >
+                    Add
+                </button>
             </div>
         )
     }
